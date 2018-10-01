3267506874f8535f62c1b4d7b3d78b55129ce681
Nick Pullen <nickpullen@nicks-macbook-pro.local>
2018-10-01 12:52:27 -0700

    2.12 Completed

---
+++ You are Awesome!/Types.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var myInt = 30000
+var myDouble = 30000.0
+var myBool = true
+
+var greeting = "Hello, "
+var name = "Developer"
+var greetingString = greeting + name
+print(greetingString)
+
+var kilometers: Double = 5.0
+var kmInMiles = 1.609
+
+// greetingString = greetingString + kilometers
+var howMuchIRan = kilometers / kmInMiles
--- You are Awesome!/ViewController.swift
+++ You are Awesome!/ViewController.swift
@@ -11,21 +11,60 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
+    var index = 0
+
     
     
     //Code below executes when the app's view below first loads
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    @IBAction func showMessagePressed(_ sender: UIButton) {
-        messageLabel.text = "You are Awesome!"
-        messageLabel.textColor = UIColor.red
         
     }
+    @IBAction func showMessagePressed(_ sender: UIButton) {
+        
+        let messages = ["You Are Awesome!",
+                        "You Are Great!",
+                        "You Are Fantastic!",
+                        "When the Genuis Bar needs help, they call you!",
+                        "You Brighten My Day!",
+                        "You are da bomb!",
+                        "Hey, fabulous",
+                        "You are tremendous!",
+                        "You've got the design skills of Jonny Ive!",
+                        "I can't wait to download you app!"]
+        messageLabel.text = messages.randomElement()!
+        
+        
+        
+        
+        
+//        messageLabel.text = messages[index]
+//
+//        if index == messages.count - 1 {
+//            index = 0
+//        } else {
+//            index = index + 1
+//        }
+        // loops the array back to the beginning, would crash if you didn't use this
+        
+        
+        
+        
+//        let message1 = "You Are Awesome!"
+//        let message2 = "You Are Great!"
+//        let message3 = "You Are Amazing"
+//
+//        if messageLabel.text == message1 {
+//            messageLabel.text = message2
+//        } else if messageLabel.text == message2 {
+//            messageLabel.text = message3
+//        } else {
+//            messageLabel.text = message1
+//        }
+
     
-    @IBAction func showAnotherMessagePressed(_ sender: UIButton) {
-        messageLabel.text = "You are Great!"
-        messageLabel.textColor = UIColor.blue
     }
+    
+
 }
 
